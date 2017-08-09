dfd4ac87fc2fbf0cea5b1d79de98ccce100ef4e7
ssc <1033729276@qq.com>
2017-08-09 22:24:52 +0800

    全部加载load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    livereload = require('gulp-livereload');
+var gulp = require('gulp');
+    // less = require('gulp-less'),
+    // livereload = require('gulp-livereload');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('less',function(){
   gulp.src('less/*.less')
-    .pipe(less())
+    .pipe($.less())
     .pipe(gulp.dest('build/css'))
 });
 
