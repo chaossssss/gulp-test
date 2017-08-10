0db2668ad7f533c0841e87bd322dab4226b53f39
ssc <1033729276@qq.com>
2017-08-10 21:49:45 +0800

    实现sass编译，但无法实现同步浏览器

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,43 @@ var gulp = require('gulp');
     // less = require('gulp-less'),
     // livereload = require('gulp-livereload');
 var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
 
 gulp.task('less',function(){
   gulp.src('less/*.less')
     .pipe($.less())
     .pipe(gulp.dest('build/css'))
 });
+gulp.task('sass',function() {
+  return gulp.src('scss/*.scss')
+    .pipe($.sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({stream:true}));
+});
 
 
-gulp.task('watch',function(){
+// gulp.task('browser-sync',function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './'
+//     }
+//   });
+// });
+
+
+
+gulp.task('watch', ['sass','less'],function(){
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
   gulp.watch('less/*.less', ['less']);
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('*.html').on('change',reload);
 })
 
 
